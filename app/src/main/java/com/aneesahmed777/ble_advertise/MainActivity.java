b62383de54fcf62f807a7d2074dce8f086e57374
Anees Ahmed <aneesahmed777@gmail.com>
2018-04-21 04:22:47 +0530

    [redesign] new better infrastructure skeleton (app non functional)

--- app/src/main/java/com/aneesahmed777/ble_advertise/MainActivity.java
+++ app/src/main/java/com/aneesahmed777/ble_advertise/MainActivity.java
@@ -13,10 +13,11 @@ import android.support.design.widget.Snackbar;
 
 import java.util.Calendar;
 
-final class MainBroadcastReceiver extends BroadcastReceiver {
+class MainBroadcastReceiver extends BroadcastReceiver {
     private static final MainBroadcastReceiver theBroadcastReceiver;
 
     private static Boolean getInstance_hasBeenCalledAtleastOnce = false;
+    private MainActivity notifyActivity = null;
 
     static {
         theBroadcastReceiver = new MainBroadcastReceiver();
@@ -36,9 +37,16 @@ final class MainBroadcastReceiver extends BroadcastReceiver {
         return getInstance_hasBeenCalledAtleastOnce;
     }
 
+    public void setNotifyActivity(MainActivity activity) {
+        Log.d("MainBroadcastReceiver", "setNotifyActivity()");
+        notifyActivity = activity;
+    }
+
     public void onReceive(Context context, Intent intent) {
-        String status = intent.getStringExtra(context.getString(R.string.key_status_action_broadcast_status));
-        Log.d("MainBroadcastReceiver", "onReceive: "+status);
+        Log.d("MainBroadcastReceiver", "onReceive()");
+
+        if (notifyActivity != null)
+            notifyActivity.onBroadcastReceive(intent);
     }
 }
 
@@ -50,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtvInfo;
     private Button btnStart;
     private Button btnStop;
-    private boolean working;
+    private Boolean gettingCreated;
+    private Boolean working;
     private String data;
 
     private class DoNothingOnClick implements View.OnClickListener {
@@ -101,31 +110,28 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new BtnStartListener());
         btnStop.setOnClickListener(new BtnStopListener());
 
-        txtvInfo.setText("");
+        txtvInfo.setText(R.string.initialising_activity);
+        btnStart.setEnabled(false);
         btnStop.setEnabled(false);
+        gettingCreated = true;
         working = false;
         data = null;
-
-        if (savedInstanceState != null)
-            loadStateFromBundle(savedInstanceState);
-        else {
-            Context context = getApplicationContext();
-            String sharedPrefFilename = getString(R.string.file_sharedpref_ble_advertising_info);
-            SharedPreferences sharedPref = context.getSharedPreferences(sharedPrefFilename, Context.MODE_PRIVATE);
-            working = sharedPref.getBoolean(getString(R.string.key_working_sharedpref_ble_advertising_info), false);
-            data = sharedPref.getString(getString(R.string.key_data_sharedpref_ble_advertising_info), null);
-
-            Log.d("MainActivity", "onCreate: load from sharedPref: "+working+", "+data);
-
-            btnStart.setEnabled(!working);
-            btnStop.setEnabled(working);
-            txtvInfo.setText(data);
-        }
 
         if (!MainBroadcastReceiver.getInstance_hasBeenCalledBefore()) {
             LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
             IntentFilter statusIntentFilter = new IntentFilter(getString(R.string.action_broadcast_status));
             localBroadcastManager.registerReceiver(MainBroadcastReceiver.getInstance(), statusIntentFilter);
+        }
+
+        MainBroadcastReceiver.getInstance().setNotifyActivity(this);
+
+        if (savedInstanceState != null) {
+            loadStateFromBundle(savedInstanceState);
+        } else {
+            Context context = getApplicationContext();
+            Intent intentGetInfoAboutService = new Intent(context, MyService.class)
+                    .putExtra(getString(R.string.key_command_intent_service), R.string.val_info_intent_service);
+            context.startService(intentGetInfoAboutService);
         }
     }
 
@@ -176,19 +182,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void saveStateIntoBundle(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(getString(R.string.key_gettingCreated_bundle_savedInstanceState), gettingCreated);
         savedInstanceState.putBoolean(getString(R.string.key_working_bundle_savedInstanceState), working);
         savedInstanceState.putString(getString(R.string.key_data_bundle_savedInstanceState), data);
-        Log.d("MainActivity", "saveStateIntoBundle: "+working+", "+data);
+        savedInstanceState.putCharSequence(getString(R.string.key_txtvw_info_bundle_savedInstanceState), txtvInfo.getText());
+        Log.d("MainActivity", "saveStateIntoBundle: "+ gettingCreated +", "+working+", "+data);
     }
 
     private void loadStateFromBundle(Bundle savedInstanceState) {
+        gettingCreated = savedInstanceState.getBoolean(getString(R.string.key_gettingCreated_bundle_savedInstanceState));
         working = savedInstanceState.getBoolean(getString(R.string.key_working_bundle_savedInstanceState));
         data = savedInstanceState.getString(getString(R.string.key_data_bundle_savedInstanceState));
-        Log.d("MainActivity", "loadStateFromBundle: "+working+", "+data);
+        Log.d("MainActivity", "loadStateFromBundle: "+ gettingCreated +", "+working+", "+data);
 
-        btnStart.setEnabled(!working);
-        btnStop.setEnabled(working);
-        txtvInfo.setText(data);
+        if (gettingCreated) {
+            btnStart.setEnabled(false);
+            btnStop.setEnabled(false);
+        } else {
+            btnStart.setEnabled(!working);
+            btnStop.setEnabled(working);
+        }
+
+        txtvInfo.setText(savedInstanceState.getCharSequence(getString(R.string.key_txtvw_info_bundle_savedInstanceState)));
     }
 
     private void startAdvertising() {
@@ -197,44 +212,89 @@ public class MainActivity extends AppCompatActivity {
         data = Calendar.getInstance().getTime().toString();
 
         Context context = getApplicationContext();
-        Intent intent = new Intent(context, MyService.class);
-        intent.putExtra(getString(R.string.key_data_intent_launch_MyService), data);
-        if (context.startService(intent) == null) {
-            Log.d("MainActivity", "startAdvertising: Failed to startService()");
-            txtvInfo.setText(R.string.failed_to_start_advertising);
-            return;
-        }
+        Intent intentStartAdvertising = new Intent(context, MyService.class)
+                .putExtra(getString(R.string.key_command_intent_service), R.string.val_start_intent_service)
+                .putExtra(getString(R.string.key_data_intent_service), data);
+        context.startService(intentStartAdvertising);
 
-        btnStart.setEnabled(false);
-        btnStop.setEnabled(true);
-        txtvInfo.setText(data);
-        working = true;
-
-        String msg = getString(R.string.advertising_started);
-        Snackbar snackbar = Snackbar.make(layout, msg, SNACKBAR_DURATION);
-        snackbar.setAction(android.R.string.ok, new DoNothingOnClick());
-        snackbar.show();
+        txtvInfo.setText(R.string.starting_advertisement);
     }
 
     private void stopAdvertising() {
         Log.d("MainActivity", "stopAdvertising()");
 
         Context context = getApplicationContext();
-        Intent intent = new Intent(context, MyService.class);
-        if (!context.stopService(intent)) {
-            Log.d("MainActivity", "stopAdvertising: stopService() returned false");
-            return;
+        Intent intentStopAdvertising = new Intent(context, MyService.class)
+                .putExtra(getString(R.string.key_command_intent_service), R.string.val_stop_intent_service);
+        context.startService(intentStopAdvertising);
+
+        txtvInfo.setText(R.string.stopping_advertisement);
+    }
+
+    public void onBroadcastReceive(Intent statusIntent) {
+        Integer statusResId = statusIntent.getIntExtra(getString(R.string.key_status_action_broadcast_status), 0);
+        String status = getString(statusResId);
+
+        if (gettingCreated) {
+            gettingCreated = false;
+            data = statusIntent.getStringExtra(getString(R.string.key_data_action_broadcast_status));
+            Log.d("MainActivity", "onBroadcastReceive: "+status+", "+data);
+            switch (statusResId) {
+                case R.string.val_busy_action_broadcast_status: {
+                    btnStart.setEnabled(false);
+                    btnStop.setEnabled(true);
+                    txtvInfo.setText(data);
+                    working = true;
+                    break;
+                }
+                case R.string.val_idle_action_broadcast_status: {
+                    btnStart.setEnabled(true);
+                    btnStop.setEnabled(false);
+                    txtvInfo.setText(R.string.idle);
+                    working = false;
+                    break;
+                }
+            }
+        } else {
+            Log.d("MainActivity", "onBroadcastReceive: "+status);
+            switch (statusResId) {
+                case R.string.val_start_ok_action_broadcast_status: {
+                    btnStart.setEnabled(false);
+                    btnStop.setEnabled(true);
+                    txtvInfo.setText(data);
+                    working = true;
+                    String msg = getString(R.string.advertisement_started);
+                    Snackbar snackbar = Snackbar.make(layout, msg, SNACKBAR_DURATION);
+                    snackbar.setAction(android.R.string.ok, new DoNothingOnClick());
+                    snackbar.show();
+                    break;
+                }
+                case R.string.val_stop_ok_action_broadcast_status: {
+                    btnStart.setEnabled(true);
+                    btnStop.setEnabled(false);
+                    txtvInfo.setText(R.string.idle);
+                    data = null;
+                    working = false;
+                    String msg = getString(R.string.advertisement_stopped);
+                    Snackbar snackbar = Snackbar.make(layout, msg, SNACKBAR_DURATION);
+                    snackbar.setAction(android.R.string.ok, new DoNothingOnClick());
+                    snackbar.show();
+                    break;
+                }
+                case R.string.val_start_fail_action_broadcast_status: {
+                    btnStart.setEnabled(true);
+                    btnStop.setEnabled(false);
+                    txtvInfo.setText(R.string.failed_to_start_advertisement);
+                    data = null;
+                    break;
+                }
+                case R.string.val_stop_fail_action_broadcast_status: {
+                    btnStart.setEnabled(false);
+                    btnStop.setEnabled(true);
+                    txtvInfo.setText(R.string.failed_to_stop_advertisement);
+                    break;
+                }
+            }
         }
-
-        btnStart.setEnabled(true);
-        btnStop.setEnabled(false);
-        txtvInfo.setText("");
-        data = null;
-        working = false;
-
-        String msg = getString(R.string.advertising_stopped);
-        Snackbar snackbar = Snackbar.make(layout, msg, SNACKBAR_DURATION);
-        snackbar.setAction(android.R.string.ok, new DoNothingOnClick());
-        snackbar.show();
     }
 }
--- app/src/main/java/com/aneesahmed777/ble_advertise/MyService.java
+++ app/src/main/java/com/aneesahmed777/ble_advertise/MyService.java
@@ -3,7 +3,6 @@ package com.aneesahmed777.ble_advertise;
 import android.app.*;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
@@ -46,25 +45,51 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (working)
-            Log.d("MyService", "onStartCommand() already working");
-        else
-            Log.d("MyService", "onStartCommand()");
+        Log.d("MyService", "onStartCommand()");
 
-        if (working) {
-            Intent statusIntent = new Intent(getString(R.string.action_broadcast_status));
-            statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status), getString(R.string.val_busy_action_broadcast_status));
-            localBroadcastManager.sendBroadcast(statusIntent);
-            return Service.START_STICKY;
+        Intent statusIntent = new Intent(getString(R.string.action_broadcast_status));
+
+        Integer commandResId = intent.getIntExtra(getString(R.string.key_command_intent_service), 0);
+
+        switch (commandResId) {
+            case R.string.val_start_intent_service: startAdvertising(intent, statusIntent); break;
+            case R.string.val_stop_intent_service: stopAdvertising(statusIntent); break;
+            case R.string.val_info_intent_service: reportAdvertisingInfo(statusIntent); break;
         }
 
-        data = intent.getStringExtra(getString(R.string.key_data_intent_launch_MyService));
+        localBroadcastManager.sendBroadcast(statusIntent);
+
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy()");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void startAdvertising(Intent intent, Intent statusIntent) {
+        Log.d("MyService", "startAdvertising()");
+
+        if (working) {
+            statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
+                    R.string.val_busy_action_broadcast_status);
+
+            return;
+        }
+
+        data = intent.getStringExtra(getString(R.string.key_data_intent_service));
 
         Intent targetIntent = new Intent(this, MainActivity.class);
         PendingIntent  pendingIntent = PendingIntent.getActivity(this, 0, targetIntent, 0);
         Notification notification = notificationBuilder
                 .setSmallIcon(R.drawable.ic_notification)
-                .setContentTitle(getString(R.string.advertising_active))
+                .setContentTitle(getString(R.string.advertisement_active))
                 .setContentText(data)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
@@ -73,61 +98,56 @@ public class MyService extends Service {
         startForeground(NOTIFICATION_ID, notification);
 
         working = true;
-
-        Context context = getApplicationContext();
-        String sharedPrefFilename = getString(R.string.file_sharedpref_ble_advertising_info);
-        SharedPreferences sharedPref = context.getSharedPreferences(sharedPrefFilename, Context.MODE_PRIVATE);
-        final SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean(getString(R.string.key_working_sharedpref_ble_advertising_info), working);
-        editor.putString(getString(R.string.key_data_sharedpref_ble_advertising_info), data);
-        Boolean success = editor.commit();
+        Boolean success = true;
 
         if (success) {
-            Intent statusIntent = new Intent(getString(R.string.action_broadcast_status));
             statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
-                    getString(R.string.val_ok_action_broadcast_status));
-            localBroadcastManager.sendBroadcast(statusIntent);
-            return Service.START_STICKY;
+                    R.string.val_start_ok_action_broadcast_status);
         }
         else {
-            Intent statusIntent = new Intent(getString(R.string.action_broadcast_status));
             statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
-                    getString(R.string.val_fail_action_broadcast_status));
-            localBroadcastManager.sendBroadcast(statusIntent);
+                    R.string.val_start_fail_action_broadcast_status);
+
             stopSelf();
-            return Service.START_NOT_STICKY;
         }
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d("MyService", "onDestroy()");
+    private void stopAdvertising(Intent statusIntent) {
+        Log.d("MyService", "stopAdvertising()");
 
+        Boolean success = false;
         if (working) {
             notificationManager.cancel(NOTIFICATION_ID);
-            Context context = getApplicationContext();
-            String sharedPrefFilename = getString(R.string.file_sharedpref_ble_advertising_info);
-            SharedPreferences sharedPref = context.getSharedPreferences(sharedPrefFilename, Context.MODE_PRIVATE);
-            final SharedPreferences.Editor editor = sharedPref.edit();
-            editor.clear();
-            Boolean success = editor.commit();
-
-            Intent statusIntent = new Intent(getString(R.string.action_broadcast_status));
-            if (success)
-                statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
-                        getString(R.string.val_ok_action_broadcast_status));
-            else
-                statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
-                        getString(R.string.val_fail_action_broadcast_status));
-            localBroadcastManager.sendBroadcast(statusIntent);
 
             working = false;
+            success = true;
         }
+
+        if (success) {
+            statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
+                    R.string.val_stop_ok_action_broadcast_status);
+        }
+        else {
+            statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
+                    R.string.val_stop_fail_action_broadcast_status);
+        }
+
+        stopSelf();
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    private void reportAdvertisingInfo(Intent statusIntent) {
+        Log.d("MyService", "reportAdvertisingInfo()");
+
+        if (working) {
+            statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
+                    R.string.val_busy_action_broadcast_status);
+            statusIntent.putExtra(getString(R.string.key_data_action_broadcast_status), data);
+        } else {
+            statusIntent.putExtra(getString(R.string.key_status_action_broadcast_status),
+                    R.string.val_idle_action_broadcast_status);
+        }
+
+        if (!working)
+            stopSelf();
     }
 }
